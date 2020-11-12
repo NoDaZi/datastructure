d3ebd165c1a4438adf42838171eb6c587c64cf33
노다현 <@DESKTOP-G8Q8FIU>
2020-11-12 19:55:46 +0900

    Linked List removeFirst remove size get 추가

--- src/list/linked/implementation/LinkedList.java
+++ src/list/linked/implementation/LinkedList.java
@@ -41,6 +41,83 @@ public class LinkedList {
 		}
 		
 	}
-	
+	public Node node(int index) {
+		Node x = head;
+		for(int i=0;i<index;i++) {
+			x=x.next;
+		}
+		
+		return x;
+	}
+	public void add(int k,Object input) {
+
+		if(k==0) {
+			addFirst(input);
+		}else {
+			Node temp1= node(k-1);
+			Node temp2= temp1.next;
+			Node newNode= new Node(input);
+			newNode.next=temp2;
+			temp1.next=newNode;
+			size++;
+			if(newNode.next==null);{
+				tail= newNode;
+			}
+		}
+		
+	}
+	public String toString() {
+		if(head==null) {
+			return "[]";
+		}else {
+			Node temp =head;
+			String str = "[";
+			
+			while(temp.next!=null) {
+				str += temp.data +", ";
+				temp= temp.next;
+			}
+			
+			str+=temp.data;
+			return str+"]";
+		}
+			
+	}
+	public Object removeFirst() {
+		Node temp = head;
+		head= head.next;
+		Object returnData = temp.data;
+		temp=null;
+		size--;
+		return returnData;
+	}
+	public Object remove(int k) {
+		if(k==0) {
+			return removeFirst();
+		}
+		else {
+			Node temp = node(k-1);
+			Node todoDeleted = temp.next;
+			temp.next=temp.next.next;
+			Object returnData = todoDeleted;
+			if(todoDeleted==tail)
+			{
+				tail=temp;
+			}
+			todoDeleted= null;
+			size--;
+			return returnData;
+		}
+	}
+	public Object removeLast() {
+		return remove(size-1);
+	}
+	public int size() {
+		return size;
+	}
+	public Object get(int k) {
+		Node temp = node(k);
+		return temp.data;
+	}
 	
 }
--- src/list/linked/implementation/Main.java
+++ src/list/linked/implementation/Main.java
@@ -8,9 +8,12 @@ public class Main {
 		numbers.addFirst(30);
 		numbers.addFirst(20);
 		numbers.addFirst(10);
-		numbers.addLast(10);
-		numbers.addLast(20);
-		numbers.addLast(30);
+		numbers.add(1, 25);
+		numbers.remove(2);
+		
+		System.out.println(numbers);
+		
+		
 	}
 
 }
